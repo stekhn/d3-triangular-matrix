81b0644e77b69be0c3da1dc32a40899b463cf019
Steffen Kühne <stekhn@gmail.com>
2017-05-29 22:28:06 +0200

    Add labels for rows and columns

--- matrix.js
+++ matrix.js
@@ -2,6 +2,13 @@
 
   'strict';
 
+  var margins = {
+    top: 120,
+    right: 120,
+    bottom: 0,
+    left: 0
+  };
+
   (function init() {
 
     d3.text('data.csv', function (data) {
@@ -48,15 +55,71 @@
       }
     })
 
+    // Remove empty entry from sources array;
+    sources.shift();
+
     return {
-      sources: source,
-      targets: target,
+      sources: sources,
+      targets: targets,
       edges: edges,
     };
   }
 
-  function draw(edges) {
+  function draw(data) {
 
-    
+    var chart = d3.select('.chart');
+
+    var width = parseFloat(chart.style('width'));
+    var height = parseFloat(chart.style('height'));
+
+    // var squareSize = (width - margins.right) / data.sources.length;
+    var squareSize = 20;
+
+    var svg = chart.append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    var group = svg.append('g')
+
+    var rows = group.selectAll('.rows')
+        .data(data.sources)
+        .enter()
+      .append('g')
+        .attr('class', 'row')
+        .attr('dominant-baseline', 'text-before-edge')
+        .attr('transform', function (d, i) {
+          return 'translate(0,' + (margins.top + (i * squareSize)) + ')';
+        });
+
+    rows.append('text')
+      .attr('x', width - margins.right + squareSize)
+      .attr('dy', 3)
+      .text(function (d) { return d; });
+
+    var columms = group.selectAll('.column')
+      .data(data.targets)
+      .enter()
+    .append('g')
+      .attr('class', 'column')
+      .attr('dominant-baseline', 'text-before-edge')
+      .attr('transform', function (d, i) {
+        return 'translate(' + i * squareSize + ',' + margins.top +') rotate(-90)';
+      });
+
+    columms.append('text')
+      .attr('x', 0)
+      .attr('dy', 3)
+      .text(function (d) { return d; });
+
+    var cells = rows.selectAll('.cell')
+        .data(data.targets)
+        .enter()
+      .append('rect')
+        .attr('width', squareSize - 2)
+        .attr('height', squareSize - 2)
+        .attr ('x', function (d, i) {
+          return squareSize * i;
+        })
+        .attr('fill', 'black');
   }
 })();
