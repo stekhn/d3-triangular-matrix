3e63254792fd6981880c11f817c60aa130176bb8
Steffen Kühne <stekhn@gmail.com>
2017-06-05 22:53:46 +0200

    Remove data transformation from visualization, simplify margins

--- matrix.js
+++ matrix.js
@@ -3,17 +3,11 @@
   'strict';
 
   var colors = ['#f0f9e8','#ccebc5','#a8ddb5','#7bccc4','#43a2ca','#0868ac'];
-
-  var margins = {
-    top: 120,
-    right: 120,
-    bottom: 0,
-    left: 0
-  };
+  var margin = 120;
 
   (function init() {
 
-    d3.text('data.csv', function (data) {
+    d3.json('data.json', function (data) {
 
       draw(transform(data));
     });
@@ -21,52 +15,12 @@
 
   function transform(data) {
 
-    var sources = [];
-    var targets = [];
-    var edges = [];
-
-    // Parse CSV row by row
-    d3.csv.parseRows(data, function (row, y) {
-
-      // Create sources from header
-      if (y === 0) {
-
-        for (var x = 0; x < row.length; x++) {
-
-          sources.push(row[x]);
-        }
-
-      // Create targets and edges from remaining rows
-      } else {
-
-        for (var x = 0; x < row.length; x++) {
-
-          // Check if cell is has content and was not linked yet
-          if (sources[x] && targets.indexOf(sources[x]) < 0) {
-
-            edges.push({
-              source: sources[x],
-              target: row[0],
-              weight: +row[x],
-              x: +x - 1,
-              y: +y - 1
-            });
-          }
-        }
-
-        // Add current row to list of known targets
-        targets.push(row[0]);
-      }
+    // @todo Add sorting and entitiy count here
+    data = data.sort(function (a, b) {
+      return d3.descending(a.weight, b.weight);
     });
 
-    // Remove empty entry from sources array;
-    sources.shift();
-
-    return {
-      sources: sources,
-      edges: edges,
-      targets: targets
-    };
+    return data;
   }
 
   function draw(data) {
@@ -74,26 +28,32 @@
     var chart = d3.select('.chart');
 
     var width = parseFloat(chart.style('width'));
-    var height = width;
+    var height = (width / 2) + margin;
 
-    var min = d3.min(data.edges, function (d) { return d.weight; });
-    var max = d3.max(data.edges, function (d) { return d.weight; });
+    var min = d3.min(data, function (d) { return d.weight; });
+    var max = d3.max(data, function (d) { return d.weight; });
 
     var color = d3.scale.quantile()
       .domain([min, max])
       .range(colors);
 
-    var squareDiagonal = Math.sqrt(Math.pow(data.sources.length, 2) + Math.pow(data.targets.length, 2));
-    var squareSize = (width - margins.left - margins.right) / squareDiagonal;
-    var scaleSize = squareSize * Math.min(data.sources.length, data.targets.length);
+    // @todo Calculate count from data
+    var count = 25;
+
+    var squareDiagonal = Math.sqrt(Math.pow(count, 2) + Math.pow(count, 2));
+    var squareSize = (width - margin) / squareDiagonal;
+    var scaleSize = squareSize * Math.min(count, count);
 
     var xScale = d3.scale.ordinal()
-      .domain(data.edges.map(function (d) { return d.source; }))
+      .domain(data.map(function (d) { return d.x; }))
       .rangePoints([0, scaleSize], 1);
 
     var yScale = d3.scale.ordinal()
-      .domain(data.edges.map(function (d) { return d.target; }))
+      .domain(data.map(function (d) { return d.y; }))
       .rangePoints([0, scaleSize], 1);
+
+    console.log(xScale.range());
+    console.log(yScale.range());
 
     var xAxis = d3.svg.axis()
       .scale(xScale)
@@ -110,11 +70,13 @@
       .attr('height', height);
 
     var group = svg.append('g')
-      .attr('transform', 'translate(0,' + height / 2 + ') rotate(-45)');
+      .attr('transform',
+        'translate(' + -Math.abs(squareSize) + ',' + ((scaleSize / 2) + margin) + ') ' +
+        'rotate(-45)');
 
     group.append('g')
         .attr('class', 'x-axis')
-        .attr('transform', 'translate(0,' + margins.top + ')')
+        .attr('transform', 'translate(0,' + margin + ')')
         .call(xAxis)
       .selectAll('text')
         .attr('dy', '.5em')
@@ -123,15 +85,15 @@
 
     group.append('g')
       .attr('class', 'y-axis')
-      .attr('transform', 'translate(' + scaleSize + ',' + margins.top +')')
+      .attr('transform', 'translate(' + scaleSize + ',' + margin +')')
       .call(yAxis);
 
     var cells = group.append('g')
       .attr('class', 'cells')
-      .attr('transform', 'translate(0,' + margins.top + ')');
+      .attr('transform', 'translate(0,' + margin + ')');
 
     cells.selectAll('.cell')
-        .data(data.edges)
+        .data(data)
         .enter()
       .append('rect')
         .attr('width', squareSize - 2)
